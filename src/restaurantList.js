d02574e85dcba5b10419f4b3aa49bd0534a9adb7
Austin English <englisha@uci.edu>
2018-12-14 13:05:05 -0800

    template Complete

--- src/restaurantList.js
+++ src/restaurantList.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 
-import { connect } from 'react-redux';
-import axios from 'axios';
-
 
 class RestaurantList extends Component {
     state = {
         task: ""
     }
 
+   
+
 
     render() {
         return (
             <div id='item'>
-                <h1>Restaurant List</h1>
+                <h1>Page2</h1>
             </div>
 
         );
