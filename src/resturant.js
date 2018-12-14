c13edafb2d89bd7061c82072ae9d823af1561ee7
AKounlavong <AllanKounlavong4200@gmail.com>
2018-12-14 12:58:14 -0800

    add two files

---
+++ src/resturant.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+
+class Resturant extends Component {
+  state = {
+   
+  }
+
+
+  render() {
+    return (
+      <div id='item'>
+       <h1>page 1</h1>
+      </div>
+    );
+  }
+}
+
+
+
+export default Resturant;
---
+++ src/resturantList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class Users extends Component {
+    state = {
+        task: ""
+    }
+
+   
+
+
+    render() {
+        return (
+            <div id='item'>
+                <h1>Page2</h1>
+            </div>
+
+        );
+    }
+}
+
+
+
+
+
+export default Users;
