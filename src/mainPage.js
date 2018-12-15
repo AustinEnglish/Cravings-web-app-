870c28138eda3bd9196b3296685d610dad17ff12
AKounlavong <AllanKounlavong4200@gmail.com>
2018-12-14 16:05:15 -0800

    updating app.js

--- src/mainPage.js
+++ src/mainPage.js
@@ -8,11 +8,44 @@ class MainPage extends Component {
    
   }
 
-
+  redirectToRestaurantJS = (index) => {
+    <Redirect to="/restaurant" />
+  }
+  
+  redirectToRestaurantListJS = () => {
+    <Redirect to="/resturantList" />
+  }
+  
   render() {
     return (
       <div id='item'>
        <h1>Main Page</h1>
+       <div>
+            {/* summary */}
+            <div>
+              <p>Restaurant decider app. Detailed informations on specific restaurants</p>
+            </div>
+            {/* map function for trending restaurant */}
+            <h1>Top Trending Restaurant</h1>
+            {this.state.{/*what ever state is*/}.map((restaurant, index) => {
+              return(
+                <div key={index}>
+                  <img src={Restaurant.featured_image} onClick={ () => this.redirectToRestaurantJS(index)}/>
+                </div>
+              )
+            })}
+          </div>
+          <div>
+            <h1>Find Restaurant</h1>
+            <input type="text" placeholder="Search.."/>
+            <button type="button" className="btn btn-primary" onClick={this.redirectToRestaurantListJS()}>Submit</button>
+            <select className="custom-select-md">
+              <option>dropdown</option>
+              <option value="1">some</option>
+              <option value="2">text</option>
+              <option value="3">hello</option>
+            </select>
+          </div>
       </div>
     );
   }
